cdd5a00ab77fe49b9510790ae5d4e26db28e7242
Ghawri <ishantghawri2002@gmail.com>
2025-07-18 17:43:18 +0530

    Updated Api with ekarigar1

--- src/utiles/fetchimage.js
+++ src/utiles/fetchimage.js
@@ -1,14 +1,14 @@
 // utils/fetchImageById.js
 export const fetchimage = async (id) => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_API}/wp-json/custom/v1/elementor-page/${id}`);
+    const response = await fetch(`https://staging.ekarigar.com/medunit2/wp-json/custom/v1/elementor-page/${id}`);
     
     if (!response.ok) {
       throw new Error(`Failed to fetch image with ID ${id}`);
     }
 
     const object = await response.json();
-    console.log("data  ;",object)
+    console.log("data  ;",object.data.sections)
     
     return object.data.sections || null;
     
